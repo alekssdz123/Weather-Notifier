a7bd9c5ef6669f512abb713a59d6591f49cf346f
alekssdz123 <alekssdzonsons89@gmail.com>
2025-03-22 20:08:54 +0200

    Added data that program will output to user

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 import requests
 
-API_KEY = open('api_key.key', 'r').read() # Getting api key from api_key.key file
+url = "https://api.openweathermap.org/data/2.5/forecast?lat=56.9669&lon=23.1534&appid=b1e1a0d37b486e563a13ed0ec0dce85b"
+
+def get_response(url):
+    response = requests.get(url).json()
+    return response
+
+
+def kelvin_to_celsius(kelvin):
+    celsius = kelvin - 273.15
+    return celsius
+
+response = get_response(url)
+
+temp = round(kelvin_to_celsius(response["list"][0]["main"]["temp"]), 1)
+feels_like = round(kelvin_to_celsius(response["list"][0]["main"]["feels_like"]), 1)
+humidity = response["list"][0]["main"]["humidity"]
+wind_speed = response["list"][0]["wind"]["speed"]
+description = response["list"][0]["weather"][0]["description"]
+
+
+
+
+
+
+
+print(response)
+print(temp)
+print(feels_like)
+print(humidity)
+print(wind_speed)
+print(description)
